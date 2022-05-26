fff73949c38b70ce5f7769a7c403fa9c19b45e0f
Sanket <sanketr920.com>
2022-05-27 00:38:05 +0530

    dynamic lib

---
+++ dynamic/main user/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"header.h"
+int main ()
+{
+  int a,b;
+  printf("enter two number\n");
+  scanf("%d%d",&a,&b);
+  printf("addition is %d\n",add(a,b));
+  printf("multiplication is %d\n",mul(a,b));
+  printf("division is %d\n",div(a,b));
+  return 0;
+
+}
