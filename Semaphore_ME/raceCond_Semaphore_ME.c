c91a589359d0977517e83112c8e68cbb3dc7a24f
Sanket <sanketr920.com>
2022-06-09 01:09:18 +0530

    race cond solved by semaphore

---
+++ Semaphore_ME/raceCond_Semaphore_ME.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+int count = 0;
+
+sem_t sem_count;
+
+void *inc_thread(void *arg)
+{
+    while(1)
+    {
+    sem_wait(&sem_count);
+    count++;
+    printf("Inc Thread %d\n",count);
+    sem_post(&sem_count);
+    }
+}
+
+void *dec_thread(void *arg)
+{
+    while(1)
+    {
+    sem_wait(&sem_count);
+    count--;
+    printf("Dec Thread %d\n",count);
+    sem_post(&sem_count);
+    }
+}
+
+int main()
+{
+    pthread_t inc,dec;
+
+    sem_init(&sem_count,0,1);
+
+    pthread_create(&inc,NULL,inc_thread,NULL);
+    pthread_create(&dec,NULL,dec_thread,NULL);
+    pthread_join(inc,NULL);
+    pthread_join(dec,NULL);
+
+    sem_destroy(&sem_count);
+    return 0;
+}
